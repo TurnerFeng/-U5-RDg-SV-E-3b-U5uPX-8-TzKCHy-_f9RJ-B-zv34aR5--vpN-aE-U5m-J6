8401f64eb7bca086be5676e180896f5aab29e4fa
TurnerFeng <34276723+TurnerFeng@users.noreply.github.com>
2017-12-07 13:19:11 +0100

    Add files via upload

---
+++ scanCubecolour( Kopf).m
@@ -0,0 +1,18 @@
+function []=scanCubecolour()
+%
+%###############################################
+%
+% PST-Projekt
+% 2.Abgabe
+%
+% erstellt am : 05.12.2017
+% letzte Änderung : 
+%
+% erstellt durch :
+% Bearbeiter :
+%
+%###############################################
+% Das Ziel dieser Funktion ist, die Farbe der Würfel zu erkennen.
+% Der Sensor wird drei Rohwerte zurückgeben, nachdem jedes Mal der Sensor
+% die Würfel gescannt hat. Diese Funktion kann die Farbe der Würfel aus
+% den 3 Farbewerten ermitteln.
